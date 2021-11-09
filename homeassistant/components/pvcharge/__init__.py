59d612feedf5166712f95aadb98743d41a5547e8
morphZ <franz.kirchhoff@gmail.com>
2021-11-09 21:50:12 +0000

    WIP: Switch to template tracking instead of entity tracking

--- homeassistant/components/pvcharge/__init__.py
+++ homeassistant/components/pvcharge/__init__.py
@@ -3,12 +3,52 @@ from __future__ import annotations
 
 import logging
 
+import voluptuous as vol
+
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
 from .pvcharger import PVCharger
 
 _LOGGER = logging.getLogger(__name__)
+
+CONF_BALANCE = "balance_entity"
+CONF_BALANCE_TEMPLATE = "balance_template"
+CONF_CHARGE_ENTITY = "charge_entity"
+CONF_SWITCH = "charge_switch"
+CONF_MIN = "charge_min"
+CONF_MAX = "charge_max"
+CONF_THRESHOLD = "pv_threshold"
+CONF_HYSTERESIS = "pv_hysteresis"
+CONF_DURATION = "boost_duration"
+CONF_SOC = "soc_template"
+CONF_LOW_VALUE = "soc_low_value"
+CONF_REFRESH = "refresh_interval"
+
+DEFAULT_DURATION = 30
+
+PVCHARGE_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_BALANCE_TEMPLATE): cv.template,
+        # vol.Required(CONF_CHARGE_ENTITY): cv.entity_id,
+        vol.Optional(CONF_SWITCH): cv.entity_id,
+        vol.Required(CONF_MIN): vol.Coerce(float),
+        vol.Required(CONF_MAX): vol.Coerce(float),
+        vol.Optional(CONF_THRESHOLD): vol.Coerce(float),
+        vol.Optional(CONF_HYSTERESIS): vol.Coerce(float),
+        vol.Required(CONF_DURATION, default=DEFAULT_DURATION): vol.Coerce(int),
+        vol.Optional(CONF_SOC): cv.template,
+        vol.Optional(CONF_LOW_VALUE): vol.Coerce(float),
+        vol.Optional(CONF_REFRESH): vol.Coerce(int),
+    }
+)
+
+CONFIG_SCHEMA = vol.Schema({DOMAIN: PVCHARGE_SCHEMA}, extra=vol.ALLOW_EXTRA)
+
+# CONFIG_SCHEMA = vol.Schema(
+#     {DOMAIN: cv.schema_with_slug_keys(PVCHARGE_SCHEMA)}, extra=vol.ALLOW_EXTRA
+# )
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
@@ -16,9 +56,10 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     # pylint: disable=no-member
     _LOGGER.warning("Enter async_setup")
 
-    hass.data.setdefault(DOMAIN, {})
+    # hass.data.setdefault(DOMAIN, {})
+    params = [config[DOMAIN].get(k, None) for k in PVCHARGE_SCHEMA.schema.keys()]
 
-    pvc = PVCharger(hass, config)
+    pvc = PVCharger(hass, *params)
     await pvc.auto()  # type: ignore
 
     return True
--- homeassistant/components/pvcharge/const.py
+++ homeassistant/components/pvcharge/const.py
@@ -1,5 +1,3 @@
 """Constants for the pvcharge integration."""
 
 DOMAIN = "pvcharge"
-
-REFRESH_LISTENER = "refresh_listener"
