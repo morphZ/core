f6947d9d564890ce472d7ccaa42b430a08d8f2d0
morphZ <franz.kirchhoff@gmail.com>
2021-11-10 21:48:36 +0000

    Go back to tracking entities instead of templates.

--- homeassistant/components/pvcharge/__init__.py
+++ homeassistant/components/pvcharge/__init__.py
@@ -13,31 +13,34 @@ from .pvcharger import PVCharger
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_BALANCE = "balance_entity"
-CONF_BALANCE_TEMPLATE = "balance_template"
-CONF_CHARGE_ENTITY = "charge_entity"
+CONF_BALANCE_ENTITY = "balance_entity"
+CONF_BALANCE_SETPOINT = "balance_setpoint"
+CONF_THRESHOLD = "balance_threshold"
+CONF_HYSTERESIS = "balance_hysteresis"
 CONF_SWITCH = "charge_switch"
 CONF_MIN = "charge_min"
 CONF_MAX = "charge_max"
-CONF_THRESHOLD = "pv_threshold"
-CONF_HYSTERESIS = "pv_hysteresis"
 CONF_DURATION = "boost_duration"
-CONF_SOC = "soc_template"
+CONF_SOC_ENTITY = "soc_entity"
 CONF_LOW_VALUE = "soc_low_value"
 CONF_REFRESH = "refresh_interval"
 
+DEFAULT_SETPOINT = 0.0
 DEFAULT_DURATION = 30
 
 PVCHARGE_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_BALANCE_TEMPLATE): cv.template,
+        vol.Required(CONF_BALANCE_ENTITY): cv.entity_id,
+        vol.Optional(CONF_BALANCE_SETPOINT, default=DEFAULT_SETPOINT): vol.Coerce(
+            float
+        ),
         vol.Optional(CONF_SWITCH): cv.entity_id,
         vol.Required(CONF_MIN): vol.Coerce(float),
         vol.Required(CONF_MAX): vol.Coerce(float),
         vol.Optional(CONF_THRESHOLD): vol.Coerce(float),
         vol.Optional(CONF_HYSTERESIS): vol.Coerce(float),
         vol.Required(CONF_DURATION, default=DEFAULT_DURATION): vol.Coerce(int),
-        vol.Optional(CONF_SOC): cv.template,
+        vol.Optional(CONF_SOC_ENTITY): cv.entity_id,
         vol.Optional(CONF_LOW_VALUE): vol.Coerce(float),
         vol.Optional(CONF_REFRESH): vol.Coerce(int),
     }
